ad4cdd29ce8fe5502fd29049cc72c51674a2b38b
Andrei Mihai <andreimihai2305is@gmail.com>
2022-07-31 20:00:48 +0300

    extracting game class from main.py file

---
+++ game.py
@@ -0,0 +1,26 @@
+import pygame
+from sys import exit
+from settings import *
+
+class Game():
+
+	def __init__(self):
+		pygame.init()
+
+		# Initilizing display
+		self.display = pygame.display.set_mode((SCREEN_SIZE, SCREEN_SIZE))
+		pygame.display.set_caption("Tic Tac Toe")
+
+	# Calling the game
+	def __call__(self) -> None:
+		self.run()
+		
+
+	# Main Game loop
+	def run(self):
+		while True:
+			for event in pygame.event.get():
+				if event.type == pygame.QUIT:
+					pygame.quit()
+					exit()
+					
